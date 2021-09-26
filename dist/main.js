a1c268825402ab11299916a50fe338c6713125ab
Rahul Patel <rahulxyzpatel@icloud.com>
2021-09-26 23:53:30 +1300

    Introducing interface

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,16 @@
+// Variable
 var hello = "world";
+// Function
 var getFullName = function (name, surname) {
     return name + " " + surname;
 };
 console.log(getFullName("Rahul", "Patel"));
+// Object
+var user = {
+    name: "Rahul",
+    age: 22
+};
+var user2 = {
+    name: "Patel"
+};
+console.log(user2.name);
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
+// Variable
 let hello: string = "world";
 
+// Function
 const getFullName = (name: string, surname: string) => {
   return `${name} ${surname}`;
 };
-
 console.log(getFullName("Rahul", "Patel"));
+
+// Object
+const user: { name: string; age: number } = {
+  name: "Rahul",
+  age: 22,
+};
+
+// Interface
+interface User {
+  name: string;
+  age?: number;
+}
+
+const user2: User = {
+  name: "Patel",
+  // age: 22,
+};
+
+console.log(user2.name);
